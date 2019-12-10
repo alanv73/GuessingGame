433e2a9724c9ec71c4089c06b8c2c627534c001f
Alan Van Art <alanv73@gmail.com>
2019-12-10 13:54:49 -0500

    initial commit

--- guessingGame/ViewController.swift
+++ guessingGame/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblOutput: UILabel!
+    @IBOutlet weak var txtGuess: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lblOutput.text = ""
     }
 
-
+    @IBAction func btnGuess(_ sender: Any) {
+        let secret = arc4random_uniform(5) + 1
+        let guess = UInt32(txtGuess.text ?? "0")
+        let message: String
+        
+        if guess == secret {
+            message = "Correct!"
+        } else {
+            message = "Sorry, I am holding \(secret) fingers up"
+        }
+        
+        lblOutput.text = message
+    }
+    
 }
 
