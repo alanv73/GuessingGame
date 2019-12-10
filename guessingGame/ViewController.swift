aa187a7487f8126b27b42ecfc8e21507772e3041
Alan Van Art <alanv73@gmail.com>
2019-12-10 13:29:47 -0500

    Initial Commit

---
+++ guessingGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  guessingGame
+//
+//  Created by Alan Van Art on 12/10/19.
+//  Copyright © 2019 Alan Van Art. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
